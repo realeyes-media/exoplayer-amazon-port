e81e6ee478d8c5733e73c6d125390d24b7f327f4
ninjapretzel <ninjapretzel@gmail.com>
2018-10-17 11:12:41 -0600

    Original: Author: Ben Ashby <mail@benashby.com> Date: 5/8/2018 12:30:40 PM Message: using cached thread pool
    ----
    Modified: library/core/src/main/java/com/google/android/exoplayer2/upstream/Loader.java

--- library/core/src/main/java/com/google/android/exoplayer2/upstream/Loader.java
+++ library/core/src/main/java/com/google/android/exoplayer2/upstream/Loader.java
@@ -21,6 +21,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.SystemClock;
 import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import com.google.android.exoplayer2.util.Assertions;
@@ -30,6 +31,8 @@ import java.io.IOException;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 /**
  * Manages the background loading of {@link Loadable}s.
@@ -148,8 +151,13 @@ public final class Loader implements LoaderErrorThrower {
   /**
    * @param threadName A name for the loader's thread.
    */
-  public Loader(String threadName) {
-    this.downloadExecutorService = Util.newSingleThreadExecutor(threadName);
+  public Loader(final String threadName) {
+    this.downloadExecutorService = Executors.newCachedThreadPool(new ThreadFactory() {
+      @Override
+      public Thread newThread(@NonNull Runnable r) {
+        return new Thread(r,threadName);
+      }
+    });
   }
 
   /**
